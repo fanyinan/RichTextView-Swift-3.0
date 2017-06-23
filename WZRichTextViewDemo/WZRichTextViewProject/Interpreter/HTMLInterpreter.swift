7f8c1549a8896b0d110a9340398b4876f7dedc12
范祎楠 <510151444@qq.com>
2017-06-23 16:02:27 +0800

    修复解析html的超链接错误的bug

--- WZRichTextViewDemo/WZRichTextViewProject/Interpreter/HTMLInterpreter.swift
+++ WZRichTextViewDemo/WZRichTextViewProject/Interpreter/HTMLInterpreter.swift
@@ -66,7 +66,7 @@ public class HTMLInterpreter: NSObject, Interpreter {
       let href = (tag as NSString).substring(with: hrefResults[0].range)
       
       //为了防止文本中有两个相同的字符串，且一个在标签内，一个在标签外，这里需要先找到标签内的文本在所有相同文本中的序号
-      let ranges = (richText.string as NSString).ranges(of: content)
+      let ranges = (text as NSString).ranges(of: content)
       let contentIndex = ranges.index(where: {$0.location == contentRangeInText.location && $0.length == contentRangeInText.length}) ?? 0
       
       contentInfos += [(content: content, index: contentIndex, href: href)]
